a4b7b10b37a2d53ecdb599be6066386a40552e51
ajithnn <ajithnn20@gmail.com>
2015-04-09 17:07:58 +0530

    Debug

--- python/grayFaceGreenEye.py
+++ python/grayFaceGreenEye.py
@@ -24,9 +24,7 @@ print fname
 b_img = cv2.imread(fname)
 siz = np.shape(b_img)
 print siz
-scaleDownFactor = int(np.sqrt(np.product(siz)/(640*640*3)))
-if scaleDownFactor == 0:
-    scaleDownFactor = 1
+scaleDownFactor = int(np.sqrt(np.product(siz)/(600*400*3)))
 nr =int(siz[0]/scaleDownFactor)
 nc = int(siz[1]/scaleDownFactor)
 img = cv2.resize(b_img, (nc, nr))
